2dbde6dba6cb754dec51c640a31c815275942ed9
Arun Rengaraman <62067339+ArunRengaraman@users.noreply.github.com>
2025-07-16 20:35:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain_community.llms import HuggingFaceHub
@@ -45,9 +45,10 @@ def prepare_knowledge_base():
 
     embeddings = load_embeddings()
 
-    vectordb = FAISS.from_documents(
+    vectordb = Chroma.from_documents(
         documents=chunks,
-        embedding=embeddings
+        embedding=embeddings,
+        persist_directory="./chroma_db"
     )
     vectordb.persist()
     return vectordb
